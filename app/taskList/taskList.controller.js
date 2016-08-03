e69e600530a3300c7a7fabde2205178be81b12cf
Michael Robinson <seraph890@gmail.com>
2016-08-03 22:16:25 +1000

    added constants for products, updated controller with scope and products

--- app/taskList/taskList.controller.js
+++ app/taskList/taskList.controller.js
@@ -8,7 +8,24 @@ angular.module('modalExercise.taskList', ['ngRoute'])
     controller: 'taskList.controller'
   });
 }])
-
-.controller('taskList.controller', [function() {
+.constant('products',
+  [{
+    key: "id",
+    value: "1",
+    key: "title",
+    value: "Product Title 1",
+    key: "description",
+    value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    key: "id",
+    value: "2",
+    key: "title",
+    value: "Product Title 2",
+    key: "description",
+    value: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }]
+)
+.controller('taskList.controller', ['$scope', 'products', function($scope, products) {
 
 }]);
