9ec0dac525922c69c3ef6b5fc52aea4553491434
Michael Robinson <seraph890@gmail.com>
2016-08-03 22:22:12 +1000

    fixed up json array names, we dont need multiple key/values

--- app/taskList/taskList.controller.js
+++ app/taskList/taskList.controller.js
@@ -10,24 +10,18 @@ angular.module('modalExercise.taskList', ['ngRoute'])
 }])
 .constant('products',
   [{
-    key: "id",
-    value: "1",
-    key: "title",
-    value: "Product Title 1",
-    key: "description",
-    value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    id: "1",
+    title: "Product Title 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: "http://placehold.it/300x300&text=img_1"
   },
   {
-    key: "id",
-    value: "2",
-    key: "title",
-    value: "Product Title 2",
-    key: "description",
-    value: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    image: "http://placehold.it/300x300&text=img_1" 
+    id: "2",
+    title: "Product Title 2",
+    description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "http://placehold.it/300x300&text=img_1"
   }]
 )
 .controller('taskList.controller', ['$scope', 'products', function($scope, products) {
-
+  $scope.products = products;
 }]);
