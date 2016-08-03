20897298ebbd79416e00c95ee2ed59a564d37583
Michael Robinson <seraph890@gmail.com>
2016-08-03 23:02:53 +1000

    added some more products to products constant

--- app/taskList/taskList.controller.js
+++ app/taskList/taskList.controller.js
@@ -20,6 +20,36 @@ angular.module('modalExercise.taskList', ['ngRoute'])
     title: "Product Title 2",
     description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: "http://placehold.it/300x300&text=img_1"
+  },
+  {
+    id: "3",
+    title: "Product Title 3",
+    description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "http://placehold.it/300x300&text=img_1"
+  },
+  {
+    id: "4",
+    title: "Product Title 4",
+    description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "http://placehold.it/300x300&text=img_1"
+  },
+  {
+    id: "5",
+    title: "Product Title 5",
+    description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "http://placehold.it/300x300&text=img_1"
+  },
+  {
+    id: "6",
+    title: "Product Title 6",
+    description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "http://placehold.it/300x300&text=img_1"
+  },
+  {
+    id: "7",
+    title: "Product Title 7",
+    description: "Bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "http://placehold.it/300x300&text=img_1"
   }]
 )
 .controller('taskList.controller', ['$scope', 'products', function($scope, products) {
